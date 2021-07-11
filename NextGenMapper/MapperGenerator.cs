4ed3a3b70656dec05733d1ea91f25a3e7d7403af
Anton Ryabchikov <aryabchikov@gotrg.com>
2021-07-11 11:50:53 +0300

    move classes to separate files

--- NextGenMapper/MapperGenerator.cs
+++ NextGenMapper/MapperGenerator.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
 namespace NextGenMapper
@@ -85,67 +83,5 @@ namespace NextGenMapper
             }
             sourceBuilder.AppendLine("};");
         }
-    }
-
-    /// <summary>
-    /// Created on demand before each generation pass
-    /// </summary>
-    class SyntaxReceiver : ISyntaxContextReceiver
-    {
-        public List<Mapping> MappingList = new List<Mapping>();
-
-        /// <summary>
-        /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
-        /// </summary>
-        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
-        {
-            if (context.Node is ClassDeclarationSyntax node)
-            {
-                var fromType = context.SemanticModel.GetDeclaredSymbol(node);
-                var attribute = fromType.GetAttributes().FirstOrDefault(x => x.AttributeClass.ToDisplayString() == Annotations.mapToAttributeName);
-                if (attribute != null)
-                {
-                    if (attribute
-                        .ConstructorArguments
-                        .SingleOrDefault()
-                        .Value is INamedTypeSymbol toType)
-                    {
-                        var reverse = toType
-                            .GetAttributes()
-                            .Any(x => x.AttributeClass.ToDisplayString() == Annotations.mapReverseAttributeName);
-                        var targetNames = fromType
-                            .GetMembers()
-                            .Where(x => x.Kind == SymbolKind.Property)
-                            .Select(x => (x.Name, TargetName: x.GetAttributes().FirstOrDefault(x => x.AttributeClass.ToDisplayString() == Annotations.targetNameAttributeName)?.ConstructorArguments.SingleOrDefault().Value as string))
-                            .Where(x => x.TargetName != null);
-                        
-                        if (reverse)
-                        {
-                            var reverseTargetNames = targetNames
-                                .Select(x => (Name: x.TargetName, TargetName: x.Name));
-                            targetNames = targetNames.Concat(reverseTargetNames);
-                        }
-
-                        var mapping = new Mapping
-                        {
-                            From = fromType,
-                            To = toType,
-                            Reverse = reverse,
-                            TargetNames = targetNames.ToDictionary(x => x.Name, y => y.TargetName)
-                        };
-                        MappingList.Add(mapping);
-                    }
-                }
-            }
-        }
-    }
-
-    class Mapping
-    {
-        public INamedTypeSymbol From { get; set; }
-        public INamedTypeSymbol To { get; set; }
-        public bool Reverse { get; set; }
-        public Dictionary<string, string> TargetNames { get; set; } = new Dictionary<string, string>();
-        public Dictionary<string, bool> Ignore { get; set; } = new Dictionary<string, bool>();
     }
 }
---
+++ NextGenMapper/Mapping.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+
+namespace NextGenMapper
+{
+    class Mapping
+    {
+        public INamedTypeSymbol From { get; set; }
+        public INamedTypeSymbol To { get; set; }
+        public bool Reverse { get; set; }
+        public Dictionary<string, string> TargetNames { get; set; } = new Dictionary<string, string>();
+    }
+}
