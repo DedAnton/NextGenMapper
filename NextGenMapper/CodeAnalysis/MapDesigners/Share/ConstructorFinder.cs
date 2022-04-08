9abdaf11c3de5c3f8539e0951ee54795fb7cba4b
Anton Ryabchikov <36799941+DedAnton@users.noreply.github.com>
2022-04-09 01:19:47 +0300

    #47 ConstructorFinder optimosations

--- NextGenMapper/CodeAnalysis/MapDesigners/Share/ConstructorFinder.cs
+++ NextGenMapper/CodeAnalysis/MapDesigners/Share/ConstructorFinder.cs
@@ -22,11 +22,17 @@ namespace NextGenMapper.CodeAnalysis.MapDesigners
 
             var fromPropertiesNames = from.GetPropertiesNames().ToImmutableHashSet(StringComparer.InvariantCultureIgnoreCase);
             var flattenPropertiesNames = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+            Span<char> flattenPropertyName = stackalloc char[128];
             foreach (var property in from.GetProperties())
             {
-                foreach (var flattenProperty in property.Type.GetProperties())
+                foreach (var nestedProperty in property.Type.GetProperties())
                 {
-                    flattenPropertiesNames.Add($"{property.Name}{flattenProperty.Name}");
+                    property.Name.AsSpan().CopyTo(flattenPropertyName);
+                    nestedProperty.Name.AsSpan().CopyTo(flattenPropertyName.Slice(property.Name.Length));
+                    //flattenPropertyName.ToString();
+                    //flattenPropertiesNames.Add($"{property.Name}{flattenProperty.Name}");
+
+                    flattenPropertiesNames.Add(flattenPropertyName.Slice(0, property.Name.Length + nestedProperty.Name.Length).ToString());
                 }
             }
 
@@ -87,6 +93,10 @@ namespace NextGenMapper.CodeAnalysis.MapDesigners
 
         public IMethodSymbol? GetOptimalUnflatteningConstructor(ITypeSymbol from, ITypeSymbol to, string unflattingPropertyName)
         {
+            if (to.SpecialType != SpecialType.None)
+            {
+                return null;
+            }
             var constructors = to.GetPublicConstructors();
             BubbleSort.SortSpan(ref constructors, _constructorComparer);
             if (constructors.Length == 0)
@@ -98,9 +108,12 @@ namespace NextGenMapper.CodeAnalysis.MapDesigners
 
             bool ValidateCommonCostructor(IMethodSymbol constructor)
             {
+                Span<char> name = stackalloc char[128];
                 foreach (var parameter in constructor.Parameters)
                 {
-                    if (!fromPropertiesNames.Contains($"{unflattingPropertyName}{parameter.Name}"))
+                    unflattingPropertyName.AsSpan().CopyTo(name);
+                    parameter.Name.AsSpan().CopyTo(name.Slice(unflattingPropertyName.Length));
+                    if (!fromPropertiesNames.Contains(name.Slice(0, unflattingPropertyName.Length+parameter.Name.Length).ToString()))
                     {
                         return false;
                     }
@@ -119,10 +132,13 @@ namespace NextGenMapper.CodeAnalysis.MapDesigners
                 }
             }
 
+            Span<char> name = stackalloc char[128];
+            unflattingPropertyName.AsSpan().CopyTo(name);
             var flattenProperties = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
             foreach(var property in to.GetProperties())
             {
-                flattenProperties.Add($"{unflattingPropertyName}{property.Name}");
+                property.Name.AsSpan().CopyTo(name.Slice(unflattingPropertyName.Length));
+                flattenProperties.Add(name.Slice(0, unflattingPropertyName.Length + property.Name.Length).ToString());
             }
 
             foreach(var property in from.GetProperties())
