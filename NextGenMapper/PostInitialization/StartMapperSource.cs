22f3094de716258709aa263a6cddee724d7b350f
KonH <KonH@yandex.ru>
2022-08-13 17:26:21 +0700

    Use internal access modifier instead of public for mappers (#53)
    * Use internal access modifier instead of public for mappers
    
    Refers to #35
    
    * Use internal access modifier also for fallback Map method
    
    Refers to #35

--- NextGenMapper/PostInitialization/StartMapperSource.cs
+++ NextGenMapper/PostInitialization/StartMapperSource.cs
@@ -8,9 +8,9 @@
 
 namespace NextGenMapper
 {
-    public static partial class Mapper
+    internal static partial class Mapper
     {
-        public static To Map<To>(this object source) => throw new InvalidOperationException($""Error when mapping {source.GetType()} to {typeof(To)}, mapping function was not found. Create custom mapping function."");
+        internal static To Map<To>(this object source) => throw new InvalidOperationException($""Error when mapping {source.GetType()} to {typeof(To)}, mapping function was not found. Create custom mapping function."");
     }
 }";
     }
