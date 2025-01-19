ba808c63144f7c3b5f5fd0048169aa4020c6b06c
Anton Ryabchikov <36799941+DedAnton@users.noreply.github.com>
2025-01-19 23:56:40 +0300

    update test

--- NextGenMapperTests/Tests/AutomaticCustomizedMapping/Properties.cs
+++ NextGenMapperTests/Tests/AutomaticCustomizedMapping/Properties.cs
@@ -93,7 +93,7 @@ public class Destination
     }
 
     [TestMethod]
-    public Task PropertyFromBaseClass_ShouldBeIgnored()
+    public Task PropertyFromBaseClass_ShouldMap()
     {
         var source =
 @"using NextGenMapper;
